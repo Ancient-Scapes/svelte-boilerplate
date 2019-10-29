65e2ebf32e1120009f0bf3816b0a86325f06eb79
SugarShootingStar <ryu.8224@gmail.com>
2019-10-29 17:05:24 +0900

    chore: add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,19 @@
 module.exports = {
-  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module'
+  },
+  env: {
+    es6: true,
+    browser: true
+  },
+  plugins: [ 'svelte3' ],
+  overrides: [
+    {
+      files: ['**/*.svelte'],
+      processor: 'svelte3/svelte3'
+    }
+  ],
   extends: 'google',
   rules: {
     'comma-dangle' : [2, 'never'],
