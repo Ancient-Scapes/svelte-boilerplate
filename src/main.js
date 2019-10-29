e8127276dea7067824aeb5a14362fb7d2531503e
SugarShootingStar <ryu.8224@gmail.com>
2019-10-29 17:11:12 +0900

    fix: eslint fix

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import App from './App.svelte';
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'world1'
-	}
+  target: document.body,
+  props: {
+    name: 'world1'
+  }
 });
 
 window.app = app;
