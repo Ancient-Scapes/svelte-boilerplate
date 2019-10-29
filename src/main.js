41a34989d5ab1891229bcc4e5b1fc274a4348379
SugarShootingStar <ryu.8224@gmail.com>
2019-10-29 17:15:55 +0900

    fix: prettier

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import App from './App.svelte';
+import App from './App.svelte'
 
 const app = new App({
   target: document.body,
   props: {
-    name: 'world1'
-  }
-});
+    name: 'world1',
+  },
+})
 
-window.app = app;
+window.app = app
 
-export default app;
+export default app
